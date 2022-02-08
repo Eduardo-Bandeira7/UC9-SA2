526fa145f3bc1ea2efb9c79f1ab2df7a30b714d3
Carlos Eduardo <carloedubf@gmail.com>
2022-02-08 19:09:06 -0300

    atualizações das atividades

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function mostarPopup() {
+    window.alert("Hello World!")
+}
+
+let = document.getElementById("campo-email");
+
+function enviarEmail() {
+    let emailDigitado = email.value;
+    console.log(emailDigitado)
+}
